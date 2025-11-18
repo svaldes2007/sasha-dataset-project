99f0993fe56aa4fdf6dca23793d69f930d60a8f2
Sasha Valdes <sashamvaldes@icloud.com>
2025-11-18 08:52:31 -0500

    changed datatype of arrayList from String to Integer

--- MentalHealthSocialMedia.java
+++ MentalHealthSocialMedia.java
@@ -17,9 +17,9 @@ public class MentalHealthSocialMedia {
         Scanner s = new Scanner(f);
         String [] header = s.nextLine().split(",");
 
-        //Find the index of "Exercise_Frequency(week)" in header → exerciseIndex
+        // a. Find the index of "Exercise_Frequency(week)" in header → exerciseIndex
         int exerciseIndex = 0;
-        // Find the index of "Stress_Level(1-10)" in header → stressIndex
+        //b. Find the index of "Stress_Level(1-10)" in header → stressIndex
         int stressIndex = 0;
         
         for (int i = 0; i < header.length; i++){
@@ -30,24 +30,27 @@ public class MentalHealthSocialMedia {
             }
         }
 
-        //2. Initialize an array of ArrayList strings with the length of 8 (index 0 = exercised 0 times per week, 1 - exercised once per week, etc) → stressList
-        ArrayList <String>[] stressList = new ArrayList <String> [8];
+        //2. Initialize an array of ArrayList integers with the length of 8 (index 0 = exercised 0 times per week, 1 - exercised once per week, etc) → stressList
+        ArrayList <Integer> [] stressList = new ArrayList <Integer> [8];
 
         //3. Go through each line of the file (while there’s a line remaining):
         while (s.hasNextLine()){
-            //Split up the line into an array of strings using → line 
+            //a. Split up the line into an array of strings using → line 
             String [] line = s.nextLine().split(",");
-            int ex = Integer.parseInt(line[exerciseIndex]);
-     
-            
-            stressList[ex].add(line[stressIndex]);
+            //b. Use line[exerciseIndex] to find the frequency of exercise → exFreq
+            int exFreq = Integer.parseInt(line[exerciseIndex]);
+            //c. Access the value of stress levels using line[stressIndex] --> stressLevel
+            int stressLevel = Integer.parseInt(line[stressIndex]);
+            //d. Add each of the stress level values to the arraylist at the index (freq) in stressList using .add()
+            stressList[exFreq].add(stressLevel);
+
         }
 
+        //4. Use a for loop to find the average of the contents of each arraylist in stressList
+        for (int i = 0; i < )
+
    
-    //     Use line[exerciseIndex] to find the frequency of exercise → freq
-    //     Access the value of stress levels using line[stressIndex]
-    //     Add each of the stress level values to the arraylist at the index (freq) in stressList using .add()
-    //     4. Use a for loop to find the average of the contents of each arraylist in stressList
+    //     
     //     Initialize a double array → correlation
     //     Use a nested for loop to set each value of correlation to the sum of all values of stressList[i] 
     //     divide by the number of values in the arrayList to get the average
